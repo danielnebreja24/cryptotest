619ee540a24a9f719e3379dacf5843f7b590bc03
danielnebreja24 <danielnebreja24@gmail.com>
2022-11-06 14:49:01 +0800

    WIP: crypto chart]

--- src/data/cryptoData.js
+++ src/data/cryptoData.js
@@ -1,4 +1,13 @@
 import Eth from "../images/eth.png";
+import Xrp from "../images/xrp.png";
+import Etc from "../images/etc.png";
+import Ltc from "../images/ltc.png";
+import Xmr from "../images/xmr.png";
+import Bsc from "../images/bsc.png";
+import Abc from "../images/abc.png";
+import Trx from "../images/trx.png";
+import Sc from "../images/sc.png";
+import Omg from "../images/omg.png";
 
 export const cryptoData = [
   {
@@ -14,62 +23,71 @@ export const cryptoData = [
     coin: "XRP",
     price: 0.00002205,
     volume: 132.691,
-    change: "+10.64"
+    change: "+10.64",
+    icon: Xrp
   },
   {
     star: false,
     coin: "ETC",
     price: 0.0007779,
     volume: 32.982,
-    change: "-6.71"
+    change: "-6.71",
+    icon: Etc
   },
   {
     star: false,
     coin: "LTC",
     price: 0.00485685,
     volume: 31.268,
-    change: "+1.88"
+    change: "+1.88",
+    icon: Ltc
   },
   {
     star: false,
     coin: "XMR",
     price: 0.00700518,
     volume: 28.567,
-    change: "-1.26"
+    change: "-1.26",
+    icon: Xmr
   },
   {
     star: false,
     coin: "BSC",
     price: 0.02105473,
     volume: 25.96,
-    change: "+0.10"
+    change: "+0.10",
+    icon: Bsc
   },
   {
     star: false,
     coin: "ABC",
     price: 0.02613303,
     volume: 21.597,
-    change: "+3.20"
+    change: "+3.20",
+    icon: Abc
   },
   {
     star: false,
     coin: "TRX",
     price: 0.00000165,
     volume: 14.106,
-    change: "-0.61"
+    change: "-0.61",
+    icon: Trx
   },
   {
     star: false,
     coin: "SC",
     price: 0.00000026,
     volume: 9.531,
-    change: "+13.04"
+    change: "+13.04",
+    icon: Sc
   },
   {
     star: false,
     coin: "OMG",
     price: 0.00019483,
     volume: 8.82,
-    change: "+3.67"
+    change: "+3.67",
+    icon: Omg
   }
 ];
